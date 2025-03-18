16a17bd6b20c4a87dbf8272bb9a2bd4c5842e422
gmzln <gamze_ya@outlook.de>
2025-03-18 16:10:27 +0100

    build Player Component

---
+++ src/components/Player.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && <input></input>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
+}
