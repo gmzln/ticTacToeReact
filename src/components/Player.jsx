40e7c0f9d24ae20d39901de11d758a130fb5b43e
gmzln <gamze_ya@outlook.de>
2025-03-24 12:31:47 +0100

    add onChange event for input field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   return (
     <li>
-      <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input></input>}
+      <span classinitial="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
